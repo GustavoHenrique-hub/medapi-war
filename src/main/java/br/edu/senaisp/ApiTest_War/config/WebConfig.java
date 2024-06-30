9e0f11cfa311546029d67896ba15f7158807cce4
unknown <gustavosilvah37@gmail.com>
2024-06-30 19:02:36 -0300

    feat: allowOrgins method

--- src/main/java/br/edu/senaisp/ApiTest_War/config/WebConfig.java
+++ src/main/java/br/edu/senaisp/ApiTest_War/config/WebConfig.java
@@ -7,11 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
-	
+public class WebConfig implements WebMvcConfigurer {
+
 	@Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-        		.allowedMethods("GET", "POST", "PUT", "DELETE");
-    }
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*");
+	}
+
 }
